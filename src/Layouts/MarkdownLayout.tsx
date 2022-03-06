06819115e8ef80803d873c69626baa92d29282da
Andrew King <pepsiotaku@gmail.com>
2022-03-06 16:19:20 -0500

    more scroll fixes

--- src/Layouts/MarkdownLayout.tsx
+++ src/Layouts/MarkdownLayout.tsx
@@ -1,8 +1,10 @@
 import debounce from "lodash/debounce";
 import { MDXRemote } from "next-mdx-remote";
 import { useRouter } from "next/router";
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import styled from "styled-components";
+
+import { Optional } from "@rewrking/react-kitchen";
 
 import { Page, SideNavigation } from "Components";
 import { mdxComponents, schemaComponents } from "Components/MarkdownComponents";
@@ -13,6 +15,7 @@ import { useUiStore } from "Stores";
 export type Props = React.PropsWithChildren<
 	ResultMDXPage & {
 		isSchema?: boolean;
+		trackScrolling?: boolean;
 	}
 >;
 
@@ -21,83 +24,79 @@ type AnchorData = {
 	id: string;
 };
 
-const MarkdownLayout = ({ meta, mdx, children, isSchema, ...navProps }: Props) => {
-	const { focusedId, setFocusedId, navOpen, heightNotifier } = useUiStore();
+const goToFocusedLink = (
+	pageLayout: Optional<HTMLDivElement>,
+	focusedId: string,
+	setFocusedId: (inValue: string) => void
+) => {
+	if (pageLayout === null) return;
+
+	const mainEl = document.getElementById("main");
+	const scrollY = mainEl?.scrollTop ?? 0;
+	const windowHeight = mainEl?.getBoundingClientRect().height ?? 0;
+	const pageLayoutHeight = pageLayout.getBoundingClientRect().height ?? 0;
+	const scrollPercent = (scrollY + windowHeight * 0.25) / pageLayoutHeight;
+
+	const links = pageLayout?.getElementsByTagName("a") ?? null;
+
+	let anchors: AnchorData[] = [];
+	if (!!links && links.length > 1) {
+		for (let i = 0; i < links.length; ++i) {
+			const dataId = links[i].getAttribute("data-id");
+			if (!!dataId) {
+				anchors.push({
+					el: links[i],
+					id: dataId,
+				});
+			}
+		}
+	}
+	if (anchors.length > 1) {
+		const firstLinkTop = anchors[0].el.offsetTop;
+
+		for (let i = 0; i < anchors.length; ++i) {
+			const { id } = anchors[i];
+			const { el } = anchors[i + 1] ?? {};
+
+			const nextTop = el?.offsetTop ?? pageLayoutHeight;
+
+			if (scrollY > firstLinkTop) {
+				if (nextTop - windowHeight * scrollPercent >= scrollY) {
+					setFocusedId(id);
+					return;
+				}
+			}
+		}
+	}
+
+	setFocusedId(focusedId.length > 0 ? focusedId : "");
+};
+
+const MarkdownLayout = ({ meta, mdx, children, isSchema, trackScrolling = true, ...navProps }: Props) => {
+	const { focusedId, setFocusedId, navOpen } = useUiStore();
 	const pageLayout = useRef<HTMLDivElement>(null);
 	const router = useRouter();
 
 	useRouteChangeScroll();
 
-	const setFocusedLink = useCallback(() => {
-		if (pageLayout.current === null) return;
-		if (router.asPath.startsWith("/schema")) {
-			// disable tracking on schema pages
-			setFocusedId("");
-			return;
-		}
-
-		const mainEl = document.getElementById("main");
-		const scrollY = mainEl?.scrollTop ?? 0;
-		const windowHeight = mainEl?.getBoundingClientRect().height ?? 0;
-		const pageLayoutHeight = pageLayout.current.getBoundingClientRect().height ?? 0;
-		const scrollPercent = (scrollY + windowHeight * 0.25) / pageLayoutHeight;
-
-		const links = pageLayout.current.getElementsByTagName("a");
-
-		if (links.length > 1) {
-			if (!!links) {
-				const firstLinkTop = links[0].offsetTop;
-
-				let arr: AnchorData[] = [];
-				for (let i = 0; i < links.length; ++i) {
-					const dataId = links[i].getAttribute("data-id");
-					if (!!dataId) {
-						arr.push({
-							el: links[i],
-							id: dataId,
-						});
-					}
-				}
-
-				for (let i = 0; i < arr.length; ++i) {
-					const { id } = arr[i];
-					const { el } = arr[i + 1] ?? {};
-
-					const nextTop = el?.offsetTop ?? pageLayoutHeight;
-
-					if (scrollY > firstLinkTop) {
-						if (nextTop - windowHeight * scrollPercent >= scrollY) {
-							setFocusedId(id);
-							return;
-						}
-					}
-				}
-			}
-		}
-
-		setFocusedId("");
-	}, [pageLayout.current]);
-
-	useWheelScroll(
-		debounce((ev) => {
-			if (navOpen) {
-				setFocusedLink();
-			}
-		}, 15),
-		[navOpen, heightNotifier, setFocusedLink]
-	);
-
 	useEffect(() => {
-		if (navOpen) {
-			setFocusedLink();
+		if (navOpen && trackScrolling) {
+			const id = typeof router.query.id === "string" ? router.query.id : focusedId;
+			goToFocusedLink(pageLayout.current, id, setFocusedId);
 		}
-	}, [navOpen, focusedId, heightNotifier, setFocusedLink]);
+	}, [navOpen, trackScrolling, router.query.id]);
+
+	const wheelCallback = debounce((ev) => {
+		if (navOpen && trackScrolling) {
+			goToFocusedLink(pageLayout.current, focusedId, setFocusedId);
+		}
+	}, 15);
 
 	return (
 		<>
 			{!!navProps.sidebarLinks && <SideNavigation {...navProps} />}
 			<Page title={meta?.title ?? "Untitled"}>
-				<Styles ref={pageLayout}>
+				<Styles ref={pageLayout} onWheel={wheelCallback}>
 					{children}
 					{!!mdx && (
 						<MDXRemote {...mdx} components={(!!isSchema ? schemaComponents : mdxComponents) as any} />
