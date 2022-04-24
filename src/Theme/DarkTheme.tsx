7ef9eda0d069fb176d568cb0d015fea46e6d0fb4
Andrew King <pepsiotaku@gmail.com>
2022-04-24 09:44:30 -0400

    lighten header color in dark theme a bit

--- src/Theme/DarkTheme.tsx
+++ src/Theme/DarkTheme.tsx
@@ -5,7 +5,7 @@ export const darkTheme: ThemeType = {
 	codeBackground: "#0a0a0a",
 	background: "#111111",
 	mainText: "#d9d9d9",
-	header: "#373346",
+	header: "#474351",
 	border: "#1d1d24",
 	primaryColor: "#f49309",
 	secondaryColor: "#f6bb5e",
