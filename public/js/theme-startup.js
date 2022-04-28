a68b263522e2cd467c4fc9f7b006129f49297455
Andrew King <pepsiotaku@gmail.com>
2022-04-27 23:43:32 -0400

    minify theme-startup script

--- public/js/theme-startup.js
+++ public/js/theme-startup.js
@@ -1,9 +1,1 @@
-(function () {
-	try {
-		var t = localStorage.getItem("themeId");
-		var dm = window.matchMedia("(prefers-color-scheme: dark)").matches === true;
-		if (!t && dm) document.documentElement.classList.add("dark");
-		if (!t) return;
-		document.documentElement.classList.add(t);
-	} catch (err) {}
-})();
+(function(){try{var t=localStorage.getItem("themeId");var dm=window.matchMedia("(prefers-color-scheme: dark)").matches===true;if(!t&&dm)document.documentElement.classList.add("dark");if(!t)return;document.documentElement.classList.add(t);}catch(err){}})();
