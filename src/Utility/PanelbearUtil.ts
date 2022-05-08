069a27f502642f7742afff8cff6a623adbf44f3f
Andrew King <pepsiotaku@gmail.com>
2022-05-08 10:44:25 -0400

    panelbear settings

--- src/Utility/PanelbearUtil.ts
+++ src/Utility/PanelbearUtil.ts
@@ -10,7 +10,7 @@ const PANELBEAR_ID: string = process.env.NEXT_PUBLIC_PANELBEAR_ID ?? "";
 const config: PanelbearImpl.PanelbearConfig = {
 	debug: isDevelopment,
 	enabled: !isDevelopment,
-	honorDNT: true,
+	honorDNT: false,
 };
 const canUsePanelbear: boolean = (config.enabled ?? true) && PANELBEAR_ID.length > 0;
 
