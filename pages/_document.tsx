8122147f23540f32b05bd2f4efcf5ea987fb62c6
Andrew King <pepsiotaku@gmail.com>
2022-07-14 22:06:00 -0400

    use ServerStyleSheet from styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,38 @@
 import Document, { Html, Head, Main, NextScript, DocumentProps } from "next/document";
 import React from "react";
+import { ServerStyleSheet } from "styled-components";
 
 import { rootStyles } from "Components";
 
 type Props = DocumentProps & {};
 
 class MyDocument extends Document<Props> {
+	// Note: this gets rid of the "flicker" during first paint
+	static async getInitialProps(ctx: any) {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				),
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
+
 	render() {
 		const { slug: slugRaw } = this.props.__NEXT_DATA__.query;
 		let slug: string = typeof slugRaw === "string" ? slugRaw : slugRaw?.join("/") ?? "";
