29f58fd9ab73c6bc32213de200d151c4b7b64605
Andrew King <pepsiotaku@gmail.com>
2023-02-08 23:23:49 -0500

    remove debug logging

--- pages/schema/[[...slug]].tsx
+++ pages/schema/[[...slug]].tsx
@@ -17,65 +17,60 @@ export default SchemaExplorePage;
 
 export const getServerSideProps: GetServerSideProps<Props> = withServerErrorHandler(
 	async (ctx: GetServerSidePropsContext) => {
-		try {
-			const { slug } = ctx.query;
-			console.log(slug);
-			if (!slug || (typeof slug !== "string" && slug.length < 2)) {
-				return {
-					redirect: {
-						destination: `/schema/latest/${SchemaType.ChaletJson}`,
-						permanent: true,
-					},
-				};
-			}
-
-			if (
-				slug.length < 2 ||
-				(typeof slug !== "string" && slug[1] !== SchemaType.SettingsJson && slug[1] !== SchemaType.ChaletJson)
-			) {
-				throw new Error(`Invalid slug rquested: ${ctx.query.slug}`);
-			}
-
-			const ref: string = typeof slug === "string" ? slug : slug[0];
-
-			let definition: string = "";
-			const type = slug[1] as SchemaType;
-
-			if (typeof slug !== "string") {
-				if (slug.length > 2) {
-					definition = slug[2];
-				}
-			}
-
-			let outDestination = typeof slug === "string" ? [slug] : [...slug];
-			if (ref.startsWith("latest")) {
-				const latestTag: string = await getLatestTag();
-				outDestination[0] = latestTag;
-				return {
-					redirect: {
-						destination: `/schema/${outDestination.join("/")}`,
-						permanent: true,
-					},
-				};
-			}
-
-			const [tags, branches] = await Promise.all([getChaletTags(), getChaletBranches()]);
-			if (!tags.includes(ref) && !branches.includes(ref)) {
-				throw new Error(`Schema for ref not found: ${ref}`);
-			}
-			const page = await markdownFiles.getMdxPage("schema", {
-				ref,
-				definition,
-				type,
-			});
+		const { slug } = ctx.query;
+		if (!slug || (typeof slug !== "string" && slug.length < 2)) {
 			return {
-				props: {
-					...page,
-					isSchema: true,
+				redirect: {
+					destination: `/schema/latest/${SchemaType.ChaletJson}`,
+					permanent: true,
 				},
 			};
-		} catch (err: any) {
-			console.error(err);
 		}
+
+		if (
+			slug.length < 2 ||
+			(typeof slug !== "string" && slug[1] !== SchemaType.SettingsJson && slug[1] !== SchemaType.ChaletJson)
+		) {
+			throw new Error(`Invalid slug rquested: ${ctx.query.slug}`);
+		}
+
+		const ref: string = typeof slug === "string" ? slug : slug[0];
+
+		let definition: string = "";
+		const type = slug[1] as SchemaType;
+
+		if (typeof slug !== "string") {
+			if (slug.length > 2) {
+				definition = slug[2];
+			}
+		}
+
+		let outDestination = typeof slug === "string" ? [slug] : [...slug];
+		if (ref.startsWith("latest")) {
+			const latestTag: string = await getLatestTag();
+			outDestination[0] = latestTag;
+			return {
+				redirect: {
+					destination: `/schema/${outDestination.join("/")}`,
+					permanent: true,
+				},
+			};
+		}
+
+		const [tags, branches] = await Promise.all([getChaletTags(), getChaletBranches()]);
+		if (!tags.includes(ref) && !branches.includes(ref)) {
+			throw new Error(`Schema for ref not found: ${ref}`);
+		}
+		const page = await markdownFiles.getMdxPage("schema", {
+			ref,
+			definition,
+			type,
+		});
+		return {
+			props: {
+				...page,
+				isSchema: true,
+			},
+		};
 	}
 );
