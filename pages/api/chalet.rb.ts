2322723408073adfb71429cef9f9d4ea4e192be2
Andrew King <pepsiotaku@gmail.com>
2023-10-21 00:03:06 -0400

    add /api/chalet.rb

---
+++ pages/api/chalet.rb.ts
@@ -0,0 +1,25 @@
+import { getLatestTag } from "Server/ChaletTags";
+import { fetchFromGithub } from "Server/FetchFromGithub";
+import { middleware } from "Server/Middleware";
+import { ApiReq, ApiRes } from "Utility";
+
+const handler = middleware.use([], async (req: ApiReq, res: ApiRes<any>) => {
+	try {
+		const latestTag: string = await getLatestTag();
+		const version = latestTag.substring(1);
+
+		const url = `https://raw.githubusercontent.com/chalet-org/chalet/main/scripts/homebrew-cask/${version}/chalet.rb`;
+		const response = await fetchFromGithub(url);
+		const blob = await response.blob();
+		const text = await blob.text();
+		res.setHeader("Content-Type", "text/plain; charset=utf-8");
+		res.status(200).send(text);
+	} catch (err: any) {
+		console.error(err);
+		res.status(500).json({
+			...err,
+		});
+	}
+});
+
+export default handler;
