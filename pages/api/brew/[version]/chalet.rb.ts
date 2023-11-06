1ea6a024a6a589d196baa9ea82d07654f9b9f840
Andrew King <pepsiotaku@gmail.com>
2023-11-06 06:36:59 -0500

    homebrew template: get template from casks repo, add caching

--- pages/api/brew/[version]/chalet.rb.ts
+++ pages/api/brew/[version]/chalet.rb.ts
@@ -1,34 +1,18 @@
 import { getLatestTag } from "Server/ChaletTags";
 import { fetchFromGithub } from "Server/FetchFromGithub";
 import { middleware } from "Server/Middleware";
+import { serverCache } from "Server/ServerCache";
 import { ApiReq, ApiRes } from "Utility";
 
-const getFileContents = (version: string, sha_arm: string, sha_x86: string) => {
-	return `# Chalet Homebrew Cask (WIP)
-#
-cask "chalet" do
-	version "${version}"
-	sha256 arm: "${sha_arm}",
-	       intel: "${sha_x86}"
-	arch arm: "arm64",
-	     intel: "x86_64"
-
-	url "https://github.com/chalet-org/chalet/releases/download/v#{version}/chalet-#{arch}-apple-darwin.zip"
-	name "Chalet"
-	desc "A cross-platform project format & build tool for C/C++"
-	homepage "https://www.chalet-work.space"
-
-	livecheck do
-		url :stable
-		regex(/^v?(\d+(?:\.\d+)+)$/i)
-	end
-
-	auto_updates true
-	depends_on macos: ">= :big_sur"
-
-	binary "chalet"
-end
-`;
+const fetchTextFromGithubUrl = async (url: string) => {
+	try {
+		const response = await fetchFromGithub(url);
+		const blob = await response.blob();
+		const text = await blob.text();
+		return text;
+	} catch (err: any) {
+		throw err;
+	}
 };
 
 const handler = middleware.use([], async (req: ApiReq, res: ApiRes<any>) => {
@@ -42,10 +26,10 @@ const handler = middleware.use([], async (req: ApiReq, res: ApiRes<any>) => {
 			version = latestTag.substring(1);
 		}
 
-		const url = `https://raw.githubusercontent.com/chalet-org/chalet-homebrew-casks/main/releases/${version}.csv`;
-		const response = await fetchFromGithub(url);
-		const blob = await response.blob();
-		const text = await blob.text();
+		const text = await serverCache.get(`chalet-cask-${version}`, () => {
+			const url = `https://raw.githubusercontent.com/chalet-org/chalet-homebrew-casks/main/releases/${version}.csv`;
+			return fetchTextFromGithubUrl(url);
+		});
 		const split = text.split("\n");
 		if (split.length < 2) {
 			throw new Error("Not found");
@@ -56,8 +40,17 @@ const handler = middleware.use([], async (req: ApiReq, res: ApiRes<any>) => {
 			throw new Error("Not found");
 		}
 
+		let rubyTemplate = await serverCache.get(`chalet-cask-ruby-template`, () => {
+			const url = `https://raw.githubusercontent.com/chalet-org/chalet-homebrew-casks/main/chalet-template.rb`;
+			return fetchTextFromGithubUrl(url);
+		});
+		rubyTemplate = rubyTemplate
+			.replaceAll("${version}", version)
+			.replaceAll("${sha_arm}", line1[1])
+			.replaceAll("${sha_x64}", line2[1]);
+
 		res.setHeader("Content-Type", "text/plain; charset=utf-8");
-		res.status(200).send(getFileContents(version, line1[1], line2[1]));
+		res.status(200).send(rubyTemplate);
 	} catch (err: any) {
 		console.error(err);
 		res.status(404).json({
