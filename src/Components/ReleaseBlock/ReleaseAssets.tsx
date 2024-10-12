0dccdbcf80772019f4d816c78fd8f1165f745a1d
Andrew King <pepsiotaku@gmail.com>
2024-10-12 00:13:08 -0400

    phase out homebrew api route, update homebrew installation directions elsewhere

--- src/Components/ReleaseBlock/ReleaseAssets.tsx
+++ src/Components/ReleaseBlock/ReleaseAssets.tsx
@@ -153,25 +153,23 @@ const ReleaseAssets = ({ assets, zipball_url, tarball_url, tag_name, snapshot }:
 					<DownloadRow>
 						<Icon id="apple" size={iconSize} color={theme.codeGray} hoverColor={theme.codeGray} />
 						<DownloadSection>
-							{!snapshot && (
-								<InstallScript
-									className="asset"
-									onTouchStart={(ev) => (ev.target as any).classList.add("touch-hover")}
-									onTouchEnd={(ev) => (ev.target as any).classList.remove("touch-hover")}
-									$color={theme.codeGray}
-								>
-									<div className="bold">
-										{platform !== OperatingSystem.MacOS}Install via{" "}
-										<Link href="https://brew.sh/">Homebrew</Link>
-									</div>
-									<CodePre
-										className="small"
-										lang="bash"
-										textContent={`brew install --cask https://www.chalet-work.space/api/brew/${version}/chalet.rb`}
-										copyButton
-									/>
-								</InstallScript>
-							)}
+							<InstallScript
+								className="asset"
+								onTouchStart={(ev) => (ev.target as any).classList.add("touch-hover")}
+								onTouchEnd={(ev) => (ev.target as any).classList.remove("touch-hover")}
+								$color={theme.codeGray}
+							>
+								<div className="bold">
+									{platform !== OperatingSystem.MacOS}Install via{" "}
+									<Link href="https://brew.sh/">Homebrew</Link>
+								</div>
+								<CodePre
+									className="small"
+									lang="bash"
+									textContent={`brew tap chalet-org/chalet\nbrew install --cask chalet${snapshot ? "@dev" : ""}`}
+									copyButton
+								/>
+							</InstallScript>
 							{macos.map((data, i) => {
 								const { platform: dataPlatform, filetype, abi } = data;
 								const { browser_download_url, name } = data.asset;
